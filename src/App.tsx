55b9b8ac49a6bcc791d78eef434b1392c59b923d
Saikat Bera <work.saikatbera@gmail.com>
2025-07-07 09:30:41 +0530

    Setup React project with package.json and dependencies

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>EVO AI Chatbot</h1>
+      <p>React project is now set up!</p>
+    </div>
+  )
+}
+
+export default App
